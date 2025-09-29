59fdbb0c5b15f756ddc513b545d7e80b8d25bffa
Porquezi <juan.mozo@usantoto.edu.co>
2025-09-29 14:25:00 -0500

    sesion.htul added

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDevelop(): string {
+    return 'Desarrollado por: Juan Mozo, Juan Poveda, Johan Lopez y Julian Sanchez';
   }
 }
