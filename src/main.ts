aa4246bbc32fa8c582b1e80dbc29d266a2c39afc
Porquezi <juan.mozo@usantoto.edu.co>
2025-10-27 15:12:46 -0500

    appinsights added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import * as appInsights from 'applicationinsights';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const connectionString = process.env.APPINSIGHTS_CONNECTION_STRING;
+  if (connectionString) {
+    appInsights
+      .setup(connectionString)
+      .setAutoDependencyCorrelation(true)
+      .setAutoCollectRequests(true)
+      .setAutoCollectPerformance(true, true)
+      .setAutoCollectExceptions(true)
+      .setAutoCollectDependencies(true)
+      .setAutoCollectConsole(true, true)
+      .setUseDiskRetryCaching(true)
+      .start();
+  }
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 443;
-  await app.listen(port, '0.0.0.0'); // 👈 clave para Docker/GitHub Actions
+  await app.listen(port, '0.0.0.0'); //
 }
 bootstrap();
