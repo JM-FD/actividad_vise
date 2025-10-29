76fd07b5e08644af8cc4e9837dfd23d98234bf19
Porquezi <juan.mozo@usantoto.edu.co>
2025-10-28 21:04:56 -0500

    change in ports

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
       .start();
   }
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 443;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0'); //
 }
 bootstrap();
