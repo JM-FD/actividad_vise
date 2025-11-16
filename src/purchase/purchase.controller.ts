9808846e73aa4bc83deaed26d2fb0414be48a563
Porquezi <juan.mozo@usantoto.edu.co>
2025-11-16 13:54:47 -0500

    Changes: error in purchase

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,25 +1,19 @@
 // purchase.controller.ts
 import { Body, Controller, Post } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
-
-class PurchaseRequestDto {
-  clientId: number;
-  amount: number;
-  date: string;
-  country: string;
-}
+import { PurchaseRequestDto } from './dto/purchase-request.dto';
 
 @Controller('purchase')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
-  @Post()
+   @Post()
   makePurchase(@Body() body: PurchaseRequestDto) {
     return this.purchaseService.processPurchase(
       body.clientId,
       body.amount,
-      body.date,
-      body.country,
+      body.purchaseDate,    
+      body.purchaseCountry, 
     );
   }
 }
